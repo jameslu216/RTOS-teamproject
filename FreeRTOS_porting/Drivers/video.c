152edf8210c3d7c64a65fb9a920520587d242108
jack111331 <jack111331@gmail.com>
2020-05-16 12:06:56 -0400

    font bigger bug fix

--- FreeRTOS_porting/Drivers/video.c
+++ FreeRTOS_porting/Drivers/video.c
@@ -143,11 +143,11 @@ void drawChar(unsigned char c, int x, int y, int colour) {
 
     // FONT INCREASE VER
     //draw pixels of the character
-    for (i = 0; i < CHAR_HEIGHT; i++) {
-        for (j = 0; j < CHAR_WIDTH; j++) {
+    for (j = 0; j < CHAR_WIDTH; j++) {
+	for (i = 0; i < CHAR_HEIGHT; i++) {
             //unsigned char temp = font[c][j];
             if (fontArial[c][i][j>>3] & (1<<(j&0x7))) {
-                framebuffer[(y + i) * SCREEN_WIDTH + (x + j)] = colour;
+                framebuffer[(y + i) * SCREEN_WIDTH + (x + (j>>2))] = colour;
             }
         }
     }
